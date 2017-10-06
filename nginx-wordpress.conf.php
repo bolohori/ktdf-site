5820127550b1ac200373553a46f8a7f47e5778e4
Jeppe Hasseriis <j.hasseriis@gmail.com>
2017-10-06 22:59:12 +0200

    Tweak nginx

--- nginx-wordpress.conf.php
+++ nginx-wordpress.conf.php
@@ -37,28 +37,28 @@ location /wp-content/ {
 # This order might seem weird - this is attempted to match last if rules below fail.
 # http://wiki.nginx.org/HttpCoreModule
 location / {
+  gzip              on;
+  gzip_vary         on;
+  gzip_proxied      any;
+  gzip_min_length   1k;
+  gzip_buffers      16 8k;
+  gzip_http_version 1.1;
+  gzip_comp_level   9;
+  gzip_types        text/plain
+                    text/javascript
+                    text/css
+                    text/xml
+                    application/json
+                    application/javascript
+                    application/atom+xml
+                    application/rss+xml
+                    application/x-javascript
+                    application/xml
+                    application/xhtml+xml
+                    application/x-font-ttf
+                    image/svg+xml
+                    ;
 	try_files $uri $uri/ /index.php?$args;
-  # gzip              on;
-  # gzip_vary         on;
-  # gzip_proxied      any;
-  # gzip_min_length   1k;
-  # gzip_buffers      16 8k;
-  # gzip_http_version 1.1;
-  # gzip_comp_level   9;
-  # gzip_types        text/plain
-  #                   text/javascript
-  #                   text/css
-  #                   text/xml
-  #                   application/json
-  #                   application/javascript
-  #                   application/atom+xml
-  #                   application/rss+xml
-  #                   application/x-javascript
-  #                   application/xml
-  #                   application/xhtml+xml
-  #                   application/x-font-ttf
-  #                   image/svg+xml
-  #                   ;
 }
 
 # Add trailing slash to */wp-admin requests.
