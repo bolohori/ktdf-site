4bfad8fbc77fce489b8639d8a8801c9b3076ab39
Jeppe Hasseriis <j.hasseriis@gmail.com>
2017-12-23 21:20:19 +0100

    Move nginx expire headers directive to top.

--- nginx-wordpress.conf.php
+++ nginx-wordpress.conf.php
@@ -22,6 +22,14 @@ gzip_types        text/plain
                   image/svg+xml
                   ;
 
+
+# Directives to send expires headers and turn off 404 error logging.
+location ~* ^.+\.(ogg|ogv|svg|svgz|eot|otf|woff|mp4|ttf|rss|atom|jpg|jpeg|gif|png|ico|zip|tgz|gz|rar|bz2|doc|xls|exe|ppt|tar|mid|midi|wav|bmp|rtf)$ {
+	access_log off;
+  log_not_found off;
+  expires 30d;
+}
+
 # Global restrictions configuration file.
 # Designed to be included in any server {} block.
 location = /favicon.ico {
@@ -63,10 +71,3 @@ location / {
 
 # Add trailing slash to */wp-admin requests.
 rewrite /wp-admin$ $scheme://$host$uri/ permanent;
-
-# Directives to send expires headers and turn off 404 error logging.
-location ~* ^.+\.(ogg|ogv|svg|svgz|eot|otf|woff|mp4|ttf|rss|atom|jpg|jpeg|gif|png|ico|zip|tgz|gz|rar|bz2|doc|xls|exe|ppt|tar|mid|midi|wav|bmp|rtf)$ {
-	access_log off;
-  log_not_found off;
-  expires 30d;
-}
