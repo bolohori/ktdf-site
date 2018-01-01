e9aa2c68fd039106bfc0acd3a6af7d45b9907c9a
Jeppe Hasseriis <j.hasseriis@gmail.com>
2018-01-01 22:11:46 +0100

    Add context structure for nginx conf

--- nginx-wordpress.conf.php
+++ nginx-wordpress.conf.php
@@ -1,73 +1,80 @@
-index index.html index.php;
+http {
+  server {
+    listen <%= ENV["PORT"] %>;
+    server_name .killtowndeathfest.com;
 
-gzip              on;
-gzip_vary         on;
-gzip_proxied      any;
-gzip_min_length   1k;
-gzip_buffers      16 8k;
-gzip_http_version 1.1;
-gzip_comp_level   9;
-gzip_types        text/plain
-                  text/javascript
-                  text/css
-                  text/xml
-                  application/json
-                  application/javascript
-                  application/atom+xml
-                  application/rss+xml
-                  application/x-javascript
-                  application/xml
-                  application/xhtml+xml
-                  application/x-font-ttf
-                  image/svg+xml
-                  ;
+    index index.html index.php;
+
+    gzip              on;
+    gzip_vary         on;
+    gzip_proxied      any;
+    gzip_min_length   1k;
+    gzip_buffers      16 8k;
+    gzip_http_version 1.1;
+    gzip_comp_level   9;
+    gzip_types        text/plain
+                      text/javascript
+                      text/css
+                      text/xml
+                      application/json
+                      application/javascript
+                      application/atom+xml
+                      application/rss+xml
+                      application/x-javascript
+                      application/xml
+                      application/xhtml+xml
+                      application/x-font-ttf
+                      image/svg+xml
+                      ;
 
 
-# Directives to send expires headers and turn off 404 error logging.
-#location ~* ^.+\.(ogg|ogv|svg|svgz|eot|otf|woff|mp4|ttf|rss|atom|jpg|jpeg|gif|png|ico|zip|tgz|gz|rar|bz2|doc|xls|exe|ppt|tar|mid|midi|wav|bmp|rtf)$ {
-#  access_log off;
-#  log_not_found off;
-#  expires 30d;
-#}
+    # Directives to send expires headers and turn off 404 error logging.
+    location ~* ^.+\.(ogg|ogv|svg|svgz|eot|otf|woff|mp4|ttf|rss|atom|jpg|jpeg|gif|png|ico|zip|tgz|gz|rar|bz2|doc|xls|exe|ppt|tar|mid|midi|wav|bmp|rtf)$ {
+      access_log off;
+      log_not_found off;
+      expires 30d;
+    }
 
-# Global restrictions configuration file.
-# Designed to be included in any server {} block.
-location = /favicon.ico {
-	log_not_found off;
-	access_log off;
+    # Global restrictions configuration file.
+    # Designed to be included in any server {} block.
+    location = /favicon.ico {
+      log_not_found off;
+      access_log off;
+    }
+
+    location = /robots.txt {
+      allow all;
+      log_not_found off;
+      access_log off;
+    }
+
+    # Deny all attempts to access hidden files such as .htaccess, .htpasswd, .DS_Store (Mac).
+    # Keep logging the requests to parse later (or to pass to firewall utilities such as fail2ban)
+    location ~ /\. {
+      deny all;
+    }
+
+    # Deny access to any files with a .php extension in the uploads directory
+    # Works in sub-directory installs and also in multisite network
+    # Keep logging the requests to parse later (or to pass to firewall utilities such as fail2ban)
+    location ~* /(?:uploads|files)/.*\.php$ {
+      deny all;
+    }
+
+    location /wp-content/ {
+      root "<?=getenv("HEROKU_APP_DIR")?>";
+    }
+
+    # WordPress single site rules.
+    # Designed to be included in any server {} block.
+
+    # This order might seem weird - this is attempted to match last if rules below fail.
+    # http://wiki.nginx.org/HttpCoreModule
+    location / {
+      try_files $uri $uri/ /index.php?$args;
+    }
+
+    # Add trailing slash to */wp-admin requests.
+    rewrite /wp-admin$ $scheme://$host$uri/ permanent;
+  }
 }
-
-location = /robots.txt {
-	allow all;
-	log_not_found off;
-	access_log off;
-}
-
-# Deny all attempts to access hidden files such as .htaccess, .htpasswd, .DS_Store (Mac).
-# Keep logging the requests to parse later (or to pass to firewall utilities such as fail2ban)
-location ~ /\. {
-	deny all;
-}
-
-# Deny access to any files with a .php extension in the uploads directory
-# Works in sub-directory installs and also in multisite network
-# Keep logging the requests to parse later (or to pass to firewall utilities such as fail2ban)
-location ~* /(?:uploads|files)/.*\.php$ {
-	deny all;
-}
-
-location /wp-content/ {
-	root "<?=getenv("HEROKU_APP_DIR")?>";
-}
-
-# WordPress single site rules.
-# Designed to be included in any server {} block.
-
-# This order might seem weird - this is attempted to match last if rules below fail.
-# http://wiki.nginx.org/HttpCoreModule
-location / {
-	try_files $uri $uri/ /index.php?$args;
-}
-
-# Add trailing slash to */wp-admin requests.
-rewrite /wp-admin$ $scheme://$host$uri/ permanent;
