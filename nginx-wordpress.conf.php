fbcfec82859435228d3e5e244060518029949e31
Jeppe Hasseriis <j.hasseriis@gmail.com>
2018-01-08 22:47:52 +0100

    Try agagin

--- nginx-wordpress.conf.php
+++ nginx-wordpress.conf.php
@@ -53,6 +53,18 @@ location /wp-content/ {
 	root "<?=getenv("HEROKU_APP_DIR")?>";
 }
 
+# WordPress single site rules.
+# Designed to be included in any server {} block.
+
+# This order might seem weird - this is attempted to match last if rules below fail.
+# http://wiki.nginx.org/HttpCoreModule
+location / {
+	try_files $uri $uri/ /index.php?$args;
+}
+
+# Add trailing slash to */wp-admin requests.
+rewrite /wp-admin$ $scheme://$host$uri/ permanent;
+
 # Directives to send expires headers and turn off 404 error logging.
 #location ~* ^.+\.(ogg|ogv|svg|svgz|eot|otf|woff|mp4|ttf|rss|atom|jpg|jpeg|gif|png|ico|zip|tgz|gz|rar|bz2|doc|xls|exe|ppt|tar|mid|midi|wav|bmp|rtf)$ {
 #  access_log off;
@@ -81,16 +93,3 @@ location ~* \.(?:jpg|jpeg|gif|png|ico|bmp|svg|svgz)$ {
 #  log_not_found off;
 #  add_header Cache-Control public;
 #}
-
-
-# WordPress single site rules.
-# Designed to be included in any server {} block.
-
-# This order might seem weird - this is attempted to match last if rules below fail.
-# http://wiki.nginx.org/HttpCoreModule
-location / {
-	try_files $uri $uri/ /index.php?$args;
-}
-
-# Add trailing slash to */wp-admin requests.
-rewrite /wp-admin$ $scheme://$host$uri/ permanent;
