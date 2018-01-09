902f308b55a983d0e063ad826546f876d314c2dd
Jeppe Hasseriis     TU Web Applikationer <jehr@dr.dk>
2018-01-09 10:07:51 +0100

    change root to alias

--- nginx-wordpress.conf.php
+++ nginx-wordpress.conf.php
@@ -51,20 +51,8 @@ location ~* /(?:uploads|files)/.*\.php$ {
 }
 
 location /wp-content/ {
-	root "<?=getenv("HEROKU_APP_DIR")?>";
+	alias "<?=getenv("HEROKU_APP_DIR")?>";
 }
-
-# WordPress single site rules.
-# Designed to be included in any server {} block.
-
-# This order might seem weird - this is attempted to match last if rules below fail.
-# http://wiki.nginx.org/HttpCoreModule
-location / {
-	try_files $uri $uri/ /index.php?$args;
-}
-
-# Add trailing slash to */wp-admin requests.
-rewrite /wp-admin$ $scheme://$host$uri/ permanent;
 
 # Directives to send expires headers and turn off 404 error logging.
 #location ~* ^.+\.(ogg|ogv|svg|svgz|eot|otf|woff|mp4|ttf|rss|atom|jpg|jpeg|gif|png|ico|zip|tgz|gz|rar|bz2|doc|xls|exe|ppt|tar|mid|midi|wav|bmp|rtf)$ {
@@ -74,7 +62,7 @@ rewrite /wp-admin$ $scheme://$host$uri/ permanent;
 #}
 
 location ~* \.(?:eot|oft|ttf|woff2?)$ {
-  root "<?=getenv("HEROKU_APP_DIR")?>";
+  # root "<?=getenv("HEROKU_APP_DIR")?>";
   add_header Access-Control-Allow-Origin *;
   add_header Cache-Control public;
   expires max;
@@ -90,7 +78,7 @@ location ~* \.(?:css|js)$ {
 }
 
 location ~* \.(?:jpg|jpeg|gif|png|ico|bmp|svg|svgz)$ {
-  root "<?=getenv("HEROKU_APP_DIR")?>";
+  # root "<?=getenv("HEROKU_APP_DIR")?>";
   add_header Cache-Control public;
   expires 30d;
   access_log off;
@@ -98,9 +86,21 @@ location ~* \.(?:jpg|jpeg|gif|png|ico|bmp|svg|svgz)$ {
 }
 
 location ~* \.(?:mp3|mp4|m4a|wav|zip|doc|xls|rtf)$ {
-  root "<?=getenv("HEROKU_APP_DIR")?>";
+  # root "<?=getenv("HEROKU_APP_DIR")?>";
   add_header Cache-Control public;
   expires 90d;
   access_log off;
   log_not_found off;
 }
+
+# WordPress single site rules.
+# Designed to be included in any server {} block.
+
+# This order might seem weird - this is attempted to match last if rules below fail.
+# http://wiki.nginx.org/HttpCoreModule
+location / {
+	try_files $uri $uri/ /index.php?$args;
+}
+
+# Add trailing slash to */wp-admin requests.
+rewrite /wp-admin$ $scheme://$host$uri/ permanent;
